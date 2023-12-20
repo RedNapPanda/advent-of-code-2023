7bcbccd8ff22f59a7a0035fda68b5d5c5630a1ee
RedNapPanda <admin@richmondsteele.com>
2023-12-19 21:42:06 -0500

    Day 8 part 1 done
    I should learn to read problems more carefully....
    It was not stated the start and end...
    Took me too long to realize ZZZ was the end and even longer to realize
    AAA was the start, as both examples work without those specifics (first
    and last instead)

--- day8/day8.go
+++ day8/day8.go
@@ -1,22 +1,87 @@
 package day8
 
+import (
+	"regexp"
+)
+
+var nodeRegex = regexp.MustCompile(`(\w{3}) = \((\w{3}), (\w{3})\)`)
+
 type Node struct {
-	left  string
-	right string
+	key, left, right string
+}
+
+type Instruction struct {
+	instr string
+	index int
+}
+
+func (i *Instruction) next() byte {
+	value := i.instr[i.index]
+	i.index++
+	if i.index >= len(i.instr) {
+		i.index = 0
+	}
+	return value
 }
 
 func Part1(lines []string) int {
-	parseNodes(lines)
-	return -1
+	count := 0
+	instr, nodeMap := parseNodes(lines)
+	node := nodeMap["AAA"]
+	for {
+		side := instr.next()
+		var next string
+		switch side {
+		case 'L':
+			next = node.left
+		case 'R':
+			next = node.right
+		}
+		node = nodeMap[next]
+		count++
+
+		if node.key == "ZZZ" {
+			break
+		}
+	}
+
+	return count
 }
 
 func Part2(lines []string) int {
 	return -1
 }
 
-func parseNodes(lines []string) (string, *map[string]Node) {
-	nodes := make(map[string]Node)
-	instructions := lines[0]
+func parseNodes(lines []string) (Instruction, map[string]Node) {
+	nodeMap := make(map[string]Node)
+	instr := Instruction{
+		lines[0],
+		0,
+	}
 
-	return instructions, &nodes
+	for i := 1; i < len(lines); i++ {
+		if len(lines[i]) == 0 {
+			continue
+		}
+		groups := nodeGroups(lines[i])
+		if groups == nil {
+			continue
+		}
+		node := Node{
+			groups[0],
+			groups[1],
+			groups[2],
+		}
+		nodeMap[groups[0]] = node
+	}
+
+	return instr, nodeMap
+}
+
+func nodeGroups(line string) []string {
+	matches := nodeRegex.FindAllStringSubmatch(line, -1)
+	if len(matches) == 0 {
+		return nil
+	}
+	return matches[0][1:]
 }
--- day8/day8_test.go
+++ day8/day8_test.go
@@ -1,10 +1,10 @@
 package day8
 
 import (
-	aoc "aoc"
-	"github.com/stretchr/testify/assert"
-	"strings"
-	"testing"
+    aoc "aoc"
+    "github.com/stretchr/testify/assert"
+    "strings"
+    "testing"
 )
 
 var example1 = strings.Split(`RL
@@ -26,33 +26,33 @@ ZZZ = (ZZZ, ZZZ)
 `, "\n")
 
 func TestPart1Example1(t *testing.T) {
-	output := Part1(example1)
+    output := Part1(example1)
 
-	assert.Equal(t, 2, output)
+    assert.Equal(t, 2, output)
 }
 
 func TestPart1Example2(t *testing.T) {
-	output := Part1(example2)
+    output := Part1(example2)
 
-	assert.Equal(t, 6, output)
+    assert.Equal(t, 6, output)
 }
 
 func TestPart1(t *testing.T) {
-	input, _ := aoc.GetInputData(7)
-	output := Part1(input)
+    input, _ := aoc.GetInputData(8)
+    output := Part1(input)
 
-	assert.Equal(t, 0, output)
+    assert.Equal(t, 19783, output)
 }
 
 func TestPart2Example(t *testing.T) {
-	output := Part2(example1)
+    output := Part2(example1)
 
-	assert.Equal(t, 0, output)
+    assert.Equal(t, 0, output)
 }
 
 func TestPart2(t *testing.T) {
-	input, _ := aoc.GetInputData(7)
-	output := Part2(input)
+    input, _ := aoc.GetInputData(8)
+    output := Part2(input)
 
-	assert.Equal(t, 0, output)
+    assert.Equal(t, 0, output)
 }
