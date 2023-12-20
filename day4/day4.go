4a9f2b02b135eee7429c8de7ce071471b87b0d05
RedNapPanda <admin@richmondsteele.com>
2023-12-19 20:02:24 -0500

    Part2 example passes, but not the main data...

---
+++ day4/day4.go
@@ -0,0 +1,87 @@
+package day4
+
+import (
+	"strings"
+)
+
+type Hand struct {
+	cards   []string
+	winners []string
+}
+
+func Part1(lines []string) int {
+	sum := 0
+	for _, line := range lines {
+		hand := parseLine(line)
+		intersect := make(map[string]string)
+		for _, v := range hand.winners {
+			intersect[v] = v
+		}
+		value := 0
+		for _, v := range hand.cards {
+			if _, ok := intersect[v]; ok {
+				if value == 0 {
+					value = 1
+				} else {
+					value *= 2
+				}
+			}
+		}
+		sum += value
+	}
+	return sum
+}
+
+func Part2(lines []string) int {
+	copies := make(map[int]int)
+	sum := 0
+	for i, line := range lines {
+		hand := parseLine(line)
+		intersect := make(map[string]string)
+		for _, v := range hand.winners {
+			intersect[v] = v
+		}
+
+		incrementValue(i, 1, copies)
+		matches := 0
+		for _, v := range hand.cards {
+			if _, ok := intersect[v]; ok {
+				matches++
+			}
+		}
+		if currentCopies, ok := copies[i]; ok {
+			for j := 1; j <= matches; j++ {
+				incrementValue(i+j, currentCopies, copies)
+			}
+		}
+	}
+	for _, v := range copies {
+		sum += v
+	}
+	return sum
+}
+
+func parseLine(line string) Hand {
+	prefix := strings.Split(line, ":")
+	pipe := strings.Split(prefix[1], "|")
+	var cards, winners []string
+	for _, winner := range strings.Fields(pipe[0]) {
+		winners = append(winners, winner)
+	}
+	for _, card := range strings.Fields(pipe[1]) {
+		cards = append(cards, card)
+	}
+
+	return Hand{
+		cards:   cards,
+		winners: winners,
+	}
+}
+
+func incrementValue(i int, value int, copies map[int]int) {
+	if _, ok := copies[i]; ok {
+		copies[i] += value
+	} else {
+		copies[i] = 1
+	}
+}
---
+++ day4/day4_test.go
@@ -0,0 +1,42 @@
+package day4
+
+import (
+	aoc "aoc"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var example = []string{
+	"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+	"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+	"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+	"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+	"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+	"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+}
+
+func TestPart1Example(t *testing.T) {
+	output := Part1(example)
+
+	assert.Equal(t, 13, output)
+}
+
+func TestPart1(t *testing.T) {
+	input, _ := aoc.GetInputData(4)
+	output := Part1(input)
+
+	assert.Equal(t, 20407, output)
+}
+
+func TestPart2Example(t *testing.T) {
+	output := Part2(example)
+
+	assert.Equal(t, 30, output)
+}
+
+func TestPart2(t *testing.T) {
+	input, _ := aoc.GetInputData(4)
+	output := Part2(input)
+
+	assert.Equal(t, 30, output)
+}
