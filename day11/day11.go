87300ca93205356f2b971dee3486547a5455b18b
RedNapPanda <admin@richmondsteele.com>
2023-12-21 13:11:54 -0500

    day10 galaxy positions found and updated

--- day11/day11.go
+++ day11/day11.go
@@ -1,24 +1,53 @@
 package day11
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type coord struct {
+	x, y int
+}
 
 func Process(lines []string) int {
+	matrix, galaxies := parseUniverse(lines)
+
+	fmt.Printf("\n")
+	for _, galaxy := range galaxies {
+		fmt.Printf("%+v\n", galaxy)
+	}
+
+	fmt.Printf("\n")
+	for _, bytes := range matrix {
+		fmt.Printf("%+v\n", string(bytes))
+	}
+	return 0
+}
+
+func parseUniverse(lines []string) ([][]byte, []coord) {
+	var galaxies []coord
 	var matrix [][]byte
+	var columnsToInc []int
 	inc := 0
 	// parse rows and insert extra empty rows
 	for x, line := range lines {
 		matrix = append(matrix, make([]byte, len(line)))
 		isEmpty := true
+		nilCoord := coord{-1, -1}
+		galaxy := nilCoord
 		for y, c := range []byte(line) {
 			matrix[x+inc][y] = c
 			if c == '#' {
 				isEmpty = false
+				galaxy = coord{x + inc, y}
 			}
 		}
 		if isEmpty {
 			slice := matrix[x+inc]
 			matrix = append(matrix, slice)
 			inc++
+		}
+		if galaxy != nilCoord {
+			galaxies = append(galaxies, galaxy)
 		}
 	}
 	inc = 0
@@ -46,19 +75,22 @@ func Process(lines []string) int {
 					post := matrix[x][y:]
 					list = append(pre, post...)
 
+					columnsToInc = append(columnsToInc, y)
 				}
 				list[y] = '.'
 				matrix[x] = list
+			}
+			for i, _ := range galaxies {
+				galaxy := galaxies[i]
+				if galaxy.y > y+1 {
+					galaxy.y += 1
+					galaxies[i] = galaxy
+				}
 			}
 			skipNext = true
 			inc++
 		}
 	}
 
-	for _, bytes := range matrix {
-		fmt.Printf("%+v\n", string(bytes))
-	}
-	fmt.Printf("%d\n", len(matrix))
-
-	return 0
+	return matrix, galaxies
 }
