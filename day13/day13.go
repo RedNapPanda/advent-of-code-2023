dfb777c3892219de8a5ad4d172dd7a4f2580acdc
RedNapPanda <admin@richmondsteele.com>
2023-12-23 14:49:49 -0500

    Parser for day13

--- day13/day13.go
+++ day13/day13.go
@@ -1,1 +1,47 @@
 package day13
+
+import "fmt"
+
+func Process(lines []string) int {
+
+	// var colCache map[int]int
+	// var rowCache map[int]int
+
+	grids := parseLines(lines)
+	fmt.Printf("grids: %d\n", len(grids))
+
+	return 0
+}
+
+/*
+parseLines
+splits lines into i j k where
+
+	i => grid ordinal
+	j => row
+	k => column
+*/
+func parseLines(lines []string) [][][]byte {
+	i := 0
+	var results [][][]byte
+	var grid [][]byte
+
+	flushGrid := func() {
+		results = append(results, grid)
+		grid = [][]byte{}
+	}
+
+	for _, line := range lines {
+		grid = append(grid, make([]byte, len(lines[0])))
+		for byteIdx, b := range []byte(line) {
+			grid[i][byteIdx] = b
+		}
+		i++
+		if line == "" {
+			i = 0
+			flushGrid()
+		}
+	}
+	flushGrid()
+	return results
+}
--- day13/day13_test.go
+++ day13/day13_test.go
@@ -1,25 +1,64 @@
 package day13
 
+import (
+	aoc "aoc"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
 var example = []string{
-    "#.##..##.",
-    "..#.##.#.",
-    "##......#",
-    "##......#",
-    "..#.##.#.",
-    "..##..##.",
-    "#.#.##.#.",
-    "",
-    "#...##..#",
-    "#....#..#",
-    "..##..###",
-    "#####.##.",
-    "#####.##.",
-    "..##..###",
-    "#....#..#",
+	"#.##..##.",
+	"..#.##.#.",
+	"##......#",
+	"##......#",
+	"..#.##.#.",
+	"..##..##.",
+	"#.#.##.#.",
+	"",
+	"#...##..#",
+	"#....#..#",
+	"..##..###",
+	"#####.##.",
+	"#####.##.",
+	"..##..###",
+	"#....#..#",
 }
+
+/*
+Goal to find the reflection points that falls within the body of the data points
+
+Seems this dataset is bound to a single reflection?  At least from example input
+Doesn't seem to mention multiple reflections..
+
+1 in this 1st pattern also has nothing to reflect to
+    123456789                   1 #...##..# 1 Since 8 is missing, there's no need to check this
+        ><                      2 #....#..#v2 2 matches 7
+    #.##..##.                   3 ..##..###v3 3 matches 6
+    ..#.##.#.                   4v#####.##.v4 5 matches 5
+    ##......#                   5^#####.##.^5
+    ##......#                   6 ..##..###^6
+    ..#.##.#.                   7 #....#..#^7
+    ..##..##.
+    #.#.##.#.
+    >>>>><<<<
+    123456789
+     98765432
+*/
 
 // invertIndex
 // s := "12345678"
 // fmt.Println(s)
 // fmt.Printf("%c\n", s[0])
 // fmt.Printf("%c\n", s[len(s)-0-1])
+
+func TestPart1Example(t *testing.T) {
+	output := Process(example)
+	assert.Equal(t, 405, output)
+}
+
+func TestPart1(t *testing.T) {
+	input, _ := aoc.GetInputData(13)
+	output := Process(input)
+
+	assert.Equal(t, 6949, output)
+}
